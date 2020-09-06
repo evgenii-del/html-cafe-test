6924a41ac6e62ab9adf51a0331a3c797537b1f34
Khasanov <zkhasanovv@gmail.com>
2020-09-06 20:25:05 +0300

    Ready layout

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 10;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const inc = Math.round(target / speed);
+        if (count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCount, 50)
+        } else {
+            counter.innerText = target;
+        }
+    }
+    updateCount();
+})
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 2000
+    });
+});
